153fc8805149bd00b8191dff3d032bc39d4adc24
Mila Decker <milacdecker@gmail.com>
2021-02-27 21:05:02 -0500

    created comments model/routes

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -3,37 +3,37 @@ const $pizzaForm = document.querySelector('#pizza-form');
 const $customToppingsList = document.querySelector('#custom-toppings-list');
 
 const handleAddTopping = event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const toppingValue = document.querySelector('#new-topping').value;
+    const toppingValue = document.querySelector('#new-topping').value;
 
-  if (!toppingValue) {
-    return false;
-  }
+    if (!toppingValue) {
+      return false;
+    }
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.name = 'topping';
-  checkbox.value = toppingValue;
-  checkbox.id = toppingValue
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'topping';
+    checkbox.value = toppingValue;
+    checkbox.id = toppingValue
+      .toLowerCase()
+      .split(' ')
+      .join('-');
 
-  const label = document.createElement('label');
-  label.textContent = toppingValue;
-  label.htmlFor = toppingValue
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+    const label = document.createElement('label');
+    label.textContent = toppingValue;
+    label.htmlFor = toppingValue
+      .toLowerCase()
+      .split(' ')
+      .join('-');
 
-  const divWrapper = document.createElement('div');
+    const divWrapper = document.createElement('div');
 
-  divWrapper.appendChild(checkbox);
-  divWrapper.appendChild(label);
-  $customToppingsList.appendChild(divWrapper);
+    divWrapper.appendChild(checkbox);
+    divWrapper.appendChild(label);
+    $customToppingsList.appendChild(divWrapper);
 
-  toppingValue.value = '';
+    toppingValue.value = '';
 };
 
 const handlePizzaSubmit = event => {
