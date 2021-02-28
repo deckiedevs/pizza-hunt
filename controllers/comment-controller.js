14de55f77434d2d59b3523376f8aeb2983494cf9
Mila Decker <milacdecker@gmail.com>
2021-02-28 11:48:27 -0500

    added replies feature

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,24 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
+    // add reply to comment
+    addReply({ params, body }, res) {
+        console.log(body)
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this ID!'});
+                    return;
+                };
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
@@ -39,10 +57,21 @@ const commentController = {
             .then(dbPizzaData => {
                 if(!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this ID!' });
-                    return
+                    return;
                 }
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     }
 };
